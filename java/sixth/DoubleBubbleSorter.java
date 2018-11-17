63e6f86f9cdb6c8848fd0398fd4669734aa9fbd0
shuzhiwen <448875211@qq.com>
2018-11-17 09:59:49 +0800

    local

---
+++ java/sixth/DoubleBubbleSorter.java
@@ -0,0 +1,28 @@
+public class DoubleBubbleSorter extends BubbleSorter{
+	double[] data;
+	
+	public DoubleBubbleSorter(double a[]) {
+		data = new double[a.length];
+		System.arraycopy(a, 0, data, 0, a.length);
+		length = a.length;
+	}
+	
+	protected void swap(int index) {
+		double tmp = data[index];
+		data[index] = data[index+1];
+		data[index+1] = tmp;
+	}
+	protected boolean outOfOrder(int index) {
+		if (data[index] > data[index+1]) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void print() {
+		for (double i : data) {
+			System.out.println(i);
+		}
+	}
+}
