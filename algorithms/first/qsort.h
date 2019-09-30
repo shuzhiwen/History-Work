28dc4600cdbb2d6fd8a108b4ad30fa9f27e621ab
shuzhiwen <448875211@qq.com>
2019-09-30 20:44:23 +0900

    local

--- algorithms/first/qsort.h
+++ algorithms/first/qsort.h
@@ -1,9 +1,16 @@
 #pragma once
 
+#include <iostream>
+
+using namespace std;
+
 template <typename T>
 class QuickSort {
 public:
-  QuickSort(T * & array, int size);
+  QuickSort(T * array, int size);
+  void branch1(T * array, int low, int high);
+  void branch2(T * array, int low, int high);
+  void branch3(T * array, int low, int high);
 
 private:
   long long count[3];
