35ea7946668df46d814944ab3428fd9d4fc3fc4f
shuzhiwen <448875211@qq.com>
2020-06-22 11:13:32 +0800

    local

---
+++ java/fourth/Teacher.java
@@ -0,0 +1,8 @@
+public class Teacher implements Worker {
+    private String subject;
+    public static void main(String[] args) {        
+    }
+    public void _do() {
+        System.out.println("to give lessons");
+    }
+}
