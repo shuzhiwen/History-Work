6fce688d0942a4a63a7b73a37e47b258efffa280
shuzhiwen <448875211@qq.com>
2020-06-22 13:19:09 +0800

    local

--- java/seventh/SortHandle.java
+++
@@ -1,6 +0,0 @@
-public interface SortHandle {
-	public abstract void swap(int index);
-	public abstract boolean outOfOrder(int index);
-	public abstract int length();
-	public abstract void setArray(Object array);
-}
